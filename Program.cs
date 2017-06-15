24a458013e37f62dcc2c76403a8e5c68f65bda7a
Nick Nash <nicholas.nash@gmail.com>
2017-06-15 23:09:04 +0100

    Make experiment slightly more similar to Anderson's. Results too unstable on my laptop, generally TTAS wins with this setup, but unwilling to draw many conclusions until experimental setup better.

--- Program.cs
+++ Program.cs
@@ -11,57 +11,98 @@ namespace ConsoleApplication
             int minThreads = Int32.Parse(args[0]);
             int maxThreads = Int32.Parse(args[1]);
             int numIterations = Int32.Parse(args[2]);
-            Console.WriteLine(Stopwatch.Frequency/1e6);
-            Console.WriteLine(Stopwatch.IsHighResolution);
 
             RunTest(numIterations, new NaiveAggressiveSpinLock(), minThreads, maxThreads);            
             RunTest(numIterations, new NaiveTestAndTestSpinLock(), minThreads, maxThreads);            
+
+            RunTest(numIterations, new NaiveTestAndTestSpinLock(), minThreads, maxThreads);            
+            RunTest(numIterations, new NaiveAggressiveSpinLock(), minThreads, maxThreads);      
+            /*
+            Astonishing output: 2nd iteration of naive aggressive is quadratic in num threads! 
+            heat / throttling effect? Results generally not easily replicable.
+            minThreads=1, maxThreads=8, numIterations=100000
+
+            1 --> 190
+2 --> 249
+3 --> 377
+4 --> 527
+5 --> 804
+6 --> 1095
+7 --> 1307
+8 --> 1559
+1 --> 192
+2 --> 265
+3 --> 412
+4 --> 557
+5 --> 914
+6 --> 1239
+7 --> 1454
+8 --> 1641
+1 --> 196
+2 --> 250
+3 --> 383
+4 --> 522
+5 --> 895
+6 --> 1244
+7 --> 1511
+8 --> 1803
+1 --> 192
+2 --> 2803
+3 --> 9407
+4 --> 16251
+5 --> 25920
+6 --> 35598
+7 --> 46149
+8 --> 57302
+            */      
         }
 
         private static void RunTest(int numIterations, INaiveSpinLock naiveLock, int minThreads, int maxThreads)
         {
+            var a = new int[256];
+            Action fill = () => {
+                for(int i = 0; i < a.Length; ++i)
+                {
+                    a[i] += i;
+                }
+            };
+            Console.WriteLine($"----- Beginning Test Run {naiveLock.GetType()} minThreads={minThreads}, maxThreads={maxThreads}, numIterations={numIterations} ------");
             for(int i = minThreads; i <= maxThreads; ++i)
             {
                 var sw = new Stopwatch();
-                var barrier = new Barrier(i);
+                var barrier = new Barrier(i + 1);
                 var threads = new Thread[i];
-                sw.Start();
                 for(int j = 0; j < i; ++j)
                 {
-                    threads[j] = new Thread(new ThreadStart(GetWorkload(numIterations, naiveLock, barrier)));
+                    threads[j] = new Thread(new ThreadStart(GetWorkload(numIterations, naiveLock, barrier, fill)));
                     threads[j].Start();
                 }
-                for(int j = 0; j < i; ++j)
-                {
-                    threads[j].Join();
-                }
+                barrier.SignalAndWait();
+                sw.Start();
+                barrier.SignalAndWait();                
                 sw.Stop();
                 Console.WriteLine($"{i} --> {sw.ElapsedMilliseconds}");
             }
         }
 
-        private static Action GetWorkload(int numIterations, INaiveSpinLock naiveLock, Barrier barrier)
+        private static Action GetWorkload(int numIterations, INaiveSpinLock naiveLock, Barrier barrier, Action doIteration)
         {
             return () => 
             {
                 barrier.SignalAndWait();        
-                double x = 100, y = 200;
                 var s = new Stopwatch();
+                var r = new Random();
                 for(int i = 0; i < numIterations; ++i)
                 {
+                    naiveLock.Enter();
+                    doIteration();
+                    naiveLock.Exit();
                     s.Reset();
                     s.Start();
-                    naiveLock.Enter();/* 
-                    for(int j = 0; j < 10000 + i % 3; ++j)
-                    {
-                        x += j / y;
-                        y -= i * j;
-                    }*/
-                    while(s.ElapsedTicks < 15000) ;
-                    naiveLock.Exit();
+                    int ticks = r.Next(1500);
+                    while(s.ElapsedTicks < ticks) Thread.Sleep(0);
                 }
-                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} computed: {x}, {y}");
-                barrier.SignalAndWait();                
+                barrier.SignalAndWait();
             };
         }
     }
