9b1b863528705304fce832a64d83a02d6938a527
Nick Nash <nicholas.nash@gmail.com>
2017-08-10 20:54:27 +0100

    Match Anderson's experimental setup slightly more closely

--- Program.cs
+++ Program.cs
@@ -10,54 +10,27 @@ namespace ConsoleApplication
         {
             int minThreads = Int32.Parse(args[0]);
             int maxThreads = Int32.Parse(args[1]);
-            int numIterations = Int32.Parse(args[2]);
-
-            RunTest(numIterations, new NaiveAggressiveSpinLock(), minThreads, maxThreads);            
-            RunTest(numIterations, new NaiveTestAndTestSpinLock(), minThreads, maxThreads);            
-
-            RunTest(numIterations, new NaiveTestAndTestSpinLock(), minThreads, maxThreads);            
-            RunTest(numIterations, new NaiveAggressiveSpinLock(), minThreads, maxThreads);      
-            /*
-            Astonishing output: 2nd iteration of naive aggressive is quadratic in num threads! 
-            heat / throttling effect? Results generally not easily replicable.
-            minThreads=1, maxThreads=8, numIterations=100000
-
-            1 --> 190
-2 --> 249
-3 --> 377
-4 --> 527
-5 --> 804
-6 --> 1095
-7 --> 1307
-8 --> 1559
-1 --> 192
-2 --> 265
-3 --> 412
-4 --> 557
-5 --> 914
-6 --> 1239
-7 --> 1454
-8 --> 1641
-1 --> 196
-2 --> 250
-3 --> 383
-4 --> 522
-5 --> 895
-6 --> 1244
-7 --> 1511
-8 --> 1803
-1 --> 192
-2 --> 2803
-3 --> 9407
-4 --> 16251
-5 --> 25920
-6 --> 35598
-7 --> 46149
-8 --> 57302
-            */      
+            int numLockAcquires = Int32.Parse(args[2]);
+            int numPassesPerAcquire = 0;//Int32.Parse(args[3]);
+            var lockType = args[3];
+            
+            var naiveLock = GetLock(lockType);
+            RunTest(numLockAcquires, numPassesPerAcquire, naiveLock, minThreads, maxThreads);            
         }
 
-        private static void RunTest(int numIterations, INaiveSpinLock naiveLock, int minThreads, int maxThreads)
+        private static INaiveSpinLock GetLock(string type)
+        {
+            switch(type)
+            {
+                case "na":
+                    return new NaiveAggressiveSpinLock();
+                case "ntat":
+                    return new NaiveTestAndTestSpinLock();
+            }
+            throw new Exception($"Unknown lock type {type}");
+        }
+
+        private static void RunTest(int numLockAcquires, int numPassesPerAcquire, INaiveSpinLock naiveLock, int minThreads, int maxThreads)
         {
             var a = new int[256];
             Action fill = () => {
@@ -66,41 +39,58 @@ namespace ConsoleApplication
                     a[i] += i;
                 }
             };
-            Console.WriteLine($"----- Beginning Test Run {naiveLock.GetType()} minThreads={minThreads}, maxThreads={maxThreads}, numIterations={numIterations} ------");
+            Console.WriteLine($"----- Beginning Test Run Lock={naiveLock.GetType()} minThreads={minThreads}, maxThreads={maxThreads}, numLockAcquires={numLockAcquires}, numPassesPerAcqure={numPassesPerAcquire} ------");
+            Console.WriteLine($"NumThreads,TotalMillisecons");
             for(int i = minThreads; i <= maxThreads; ++i)
             {
                 var sw = new Stopwatch();
                 var barrier = new Barrier(i + 1);
                 var threads = new Thread[i];
+                var acquireAction = new AcquireAction(numLockAcquires, fill);
                 for(int j = 0; j < i; ++j)
                 {
-                    threads[j] = new Thread(new ThreadStart(GetWorkload(numIterations, naiveLock, barrier, fill)));
+                    threads[j] = new Thread(new ThreadStart(GetWorkload(naiveLock, barrier, acquireAction)));
                     threads[j].Start();
                 }
                 barrier.SignalAndWait();
                 sw.Start();
                 barrier.SignalAndWait();                
                 sw.Stop();
-                Console.WriteLine($"{i} --> {sw.ElapsedMilliseconds}");
+                Console.WriteLine($"{i},{sw.ElapsedMilliseconds}");
             }
         }
 
-        private static Action GetWorkload(int numIterations, INaiveSpinLock naiveLock, Barrier barrier, Action doIteration)
+        class AcquireAction
+        {
+            private readonly int _numAcquires;
+            private readonly Action _work;
+            private int _acquireCount;
+
+            public bool Finished => _acquireCount >= _numAcquires;
+
+            public AcquireAction(int numAcquires, Action work)
+            {
+                _numAcquires = numAcquires;
+                _work = work;
+            }
+
+            public void DoWork()
+            {
+                _work();
+                _acquireCount++;
+            }
+        }
+
+        private static Action GetWorkload(INaiveSpinLock naiveLock, Barrier barrier, AcquireAction acquireAction)
         {
             return () => 
             {
                 barrier.SignalAndWait();        
-                var s = new Stopwatch();
-                var r = new Random();
-                for(int i = 0; i < numIterations; ++i)
+                while(!acquireAction.Finished)
                 {
                     naiveLock.Enter();
-                    doIteration();
+                    acquireAction.DoWork(); // Don't bother double checking here, only off by numThreads acquires.
                     naiveLock.Exit();
-                    s.Reset();
-                    s.Start();
-                    int ticks = r.Next(1500);
-                    while(s.ElapsedTicks < ticks) Thread.Sleep(0);
                 }
                 barrier.SignalAndWait();
             };
