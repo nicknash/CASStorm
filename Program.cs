87bcedf364aa0973b7953ac415f411d5d9f5274d
Nick Nash <nicholas.nash@gmail.com>
2017-09-10 13:40:21 +0100

    Add stub of pure wait workload

--- Program.cs
+++ Program.cs
@@ -15,39 +15,6 @@ namespace CASStorm
         // TODO: single counter only (shouldn't be very interesting, as should be limited case / sanity check of ArrayFill)
     }
 
-    class PureWaitWorkload : IWorkload 
-    {
-        // TODO: Pure waits 
-        private static void WaitNanos(double numNanos)
-        {
-            double numTicks = 1/Stopwatch.Frequency*numNanos*1e-9;
-            var t = Stopwatch.GetTimestamp();
-            while(Stopwatch.GetTimestamp() - t < numTicks) ;
-        }
-
-        public static void Test()
-        {
-            Console.WriteLine($"1 tick = {1e6/Stopwatch.Frequency} mics");
-            for(int i = 0; i < 1000000; ++i)
-            {
-                WaitNanos(1000);
-            }
-            Console.WriteLine("wait over..");
-            for (int size = 8; size <= 1024; size <<= 1)
-            {
-                var a = new int[size];
-                var sw = new Stopwatch();
-                sw.Start();
-                int reps = 100000;
-                for (int i = 0; i < reps; ++i)
-                {
-                    Fill(a, a.Length);
-                }
-                Console.WriteLine($"Filling size {size} took: {sw.Elapsed.TotalMilliseconds/reps*1e6} nanos.");
-            }
-            return;
-        }
-    }
 */
     public class Program
     {
