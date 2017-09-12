5af055fc5d810c7f028321f142813a5b74d43682
Nick Nash <nicholas.nash@gmail.com>
2017-09-12 14:35:21 +0100

    Add fill wait workload

---
+++ Workloads/FillWaitWorkload.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CASStorm.Workloads
+{
+    class FillWaitWorkload : IWorkload
+    {
+        public string Name { get; } = "FillWait";
+        public IReadOnlyList<WorkloadEntry> Entries { get; }
+        public FillWaitWorkload(int minSizePower, int maxSizePower, int minWaitPower, int maxWaitPower)
+          {
+            var shared = new int[1 << maxSizePower];
+            var entries = new List<WorkloadEntry>();
+            for (int sizePower = minSizePower; sizePower <= maxSizePower; ++sizePower)
+            {
+                int acquireSize = 1 << sizePower;
+                Action acquireAction = () => Utils.Fill(shared, 0, acquireSize);
+                for (int waitPower = minWaitPower; waitPower <= maxWaitPower; ++waitPower)
+                {
+                    int numNanos = 1 << waitPower;
+                    Action<int> releaseAction = _ => Utils.WaitNanos(numNanos);
+                    var entry = new WorkloadEntry(acquireSize, acquireAction, numNanos, releaseAction);
+                    entries.Add(entry);
+                }
+            }
+            Entries = entries;
+        }
+    }
+}
